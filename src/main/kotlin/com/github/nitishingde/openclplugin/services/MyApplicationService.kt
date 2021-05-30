b8320cda64a72f42ec34e4abdb4de8e6e7f8def7
GitHub Action <action@github.com>
2021-05-30 06:22:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/nitishingde/openclplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nitishingde/openclplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nitishingde.openclplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nitishingde.openclplugin.MyBundle
 
 class MyApplicationService {
 
